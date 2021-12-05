2b409de10081486f80dbcc60948c88cd09530bcb
luca <2213595911@qq.com>
2021-12-05 10:01:59 +0800

    简单优化

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -2,10 +2,12 @@ import axios from 'axios'
 import config from './config'
 import { Toast } from 'vant'
 
-const { DEV_BASEURL } = config
+const { DEV_BASEURL, PDC_BASEURL } = config
+console.log(process.env.NODE_ENV)
+
 let flag: any = null
 const instance = axios.create({
-  baseURL: DEV_BASEURL,
+  baseURL: process.env.NODE_ENV === 'development' ? DEV_BASEURL : PDC_BASEURL,
   timeout: 5000,
 })
 instance.interceptors.request.use(
